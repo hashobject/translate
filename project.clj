8ebed35f01c505fa2f860df9ad5a8392412a2074
Anton Podviaznikov <podviaznikov@gmail.com>
2013-05-30 22:12:29 -0600

    add gpg-key

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject translate "0.3.0-SNAPSHOT"
+(defproject translate "0.3.0"
   :description "Translate library in Clojure. Google Translate API v2 support."
+  {:signing {:gpg-key "HashObject Ltd <team@hashobject.com>"}}
   :url "https://github.com/hashobject/translate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
