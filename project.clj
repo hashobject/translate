18dfe8a67a228c13c5bba0a265fbe90710050a25
Anton Podviaznikov <podviaznikov@gmail.com>
2013-07-16 10:58:36 -0600

    update metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject translate "0.3.2"
   :description "Translate library in Clojure. Google Translate API v2 support."
-  :signing {:gpg-key "HashObject Ltd <team@hashobject.com>"}
+  :signing {:gpg-key "Hashobject Ltd <team@hashobject.com>"}
   :url "https://github.com/hashobject/translate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
