a950b5656fde2f6fd60e56ccc9e2fc872be0ffb2
Anton Podviaznikov <podviaznikov@gmail.com>
2014-01-07 23:47:47 +0200

    update deps

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.2"]])
+                 [clj-http "0.7.8"]])
